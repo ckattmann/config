e70a92d7684d9b424a6a8e99e0b37c91a6ae2bcc
kipfer <christoph.kattmann@gmail.com>
2015-11-07 23:23:42 +0100

    Added pythonpath modification

--- copyrcs.sh
+++ copyrcs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo "--Installing Packages with apt-get"
 sudo apt-get install vim-gnome
 sudo apt-get install git
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev
 
 # Set up Vundle
+echo "--Setting up Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git config --global user.name "kipfer"
 git config --global push.default simple
@@ -13,6 +15,7 @@ git config --global push.default simple
 python ~/.vim/bundle/YouCompleteMe/install.py
 
 # Copy existing .bashrc and .vimrc
+echo "--Making Copies of .bashrc and .vimrc if necessary"
 if [ -e "~/.bashrc" ]
 then
 	echo ".bashrc exists, moving to .bashrc2"
@@ -31,4 +34,13 @@ fi
 ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.vimrc ~/.vimrc 
 
+# Add pythonlocal to PYTHONPATH
+echo "--Modifying PYTHONPATH"
+if [ ! -e "$HOME/pythonlocal" ]
+then
+    mkdir ~/pythonlocal
+fi
+export PYTHONPATH="${PYTHONPATH}:$HOME/pythonlocal"
+
+echo "--Finished, sourcing bashrc"
 source ~/.bashrc
