0c3b2ae27e2b4cd4bdf5e8e1e36ff151e96f6d9d
kipfer <christoph.kattmann@gmail.com>
2016-06-05 21:06:25 +0200

    Major update, added i3 configs

--- copyrcs.sh
+++ copyrcs.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 
-echo "--Installing Packages with apt-get"
-sudo apt-get install vim-gnome
-sudo apt-get install git
-sudo apt-get install build-essential cmake
-sudo apt-get install python-dev
 
-# Set up Vundle
-echo "--Setting up Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "-- Install and Configure git"
+sudo apt-get install -y git
 git config --global user.name "kipfer"
 git config --global push.default simple
 
-python ~/.vim/bundle/YouCompleteMe/install.py
 
-# Copy existing .bashrc and .vimrc
-echo "--Making Copies of .bashrc and .vimrc if necessary"
+echo "--Configure Terminal"
+# This path is correct for Linux Mint 17.3, might be .config/Terminal for others
+cp $(pwd)/terminalrc ~/.config/xfce4/terminal
+
+
+echo "-- Installing i3 stuff"
+sudo apt-get install -y i3 i3status dmenu
+# Install i3-gaps including all dependencies
+sudo apt-get install -y libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+make
+sudo make install
+cd ..
+
+
+echo "-- Setting Wallpaper"
+WALLPAPERLINK=https://wallpaperscraft.com/image/los_angeles_laguna_beach_buildings_skyscrapers_58395_3840x2400.jpg
+WALLPAPERLINKNOHTTPS=${WALLPAPERLINK:7}
+sudo apt-get install -y feh
+sudo apt-get install -y curl
+curl -O ~/Downloads/ $WALLPAPERLINK
+feh --bg-scale ~/Downloads/ ${WALLPAPERLINKNOTHTTPS##/*/}
+
+
+echo "-- Setting up Vim"
+if [ -e "~/.vimrc" ]
+then
+	echo ".vimrc exists, moving to .vimrc2"
+	cp ~/.vimrc ~/.vimrc2
+	rm ~/.vimrc
+fi
+ln -s $(pwd)/.vimrc ~/.vimrc 
+sudo apt-get install -y vim-gnome
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y python-dev
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+python ~/.vim/bundle/YouCompleteMe/install.py
+vim +PluginInstall +qall
+
+echo "-- Copying .bashrc into location"
 if [ -e "~/.bashrc" ]
 then
 	echo ".bashrc exists, moving to .bashrc2"
@@ -23,24 +56,17 @@ then
 	rm ~/.bashrc
 fi
 
-if [ -e "~/.vimrc" ]
-then
-	echo ".vimrc exists, moving to .vimrc2"
-	cp ~/.vimrc ~/.vimrc2
-	rm ~/.vimrc
-fi
 
 # Create symbolic links to rc's in version controlled directory
 ln -s $(pwd)/.bashrc ~/.bashrc
-ln -s $(pwd)/.vimrc ~/.vimrc 
 
-# Add pythonlocal to PYTHONPATH
-echo "--Modifying PYTHONPATH"
-if [ ! -e "$HOME/pythonlocal" ]
-then
-    mkdir ~/pythonlocal
-fi
-export PYTHONPATH="${PYTHONPATH}:$HOME/pythonlocal"
+
+echo "-- Install the arc theme <- when Linux Mint updates to Ubuntu 16 LTS"
+# sudo apt-get install -y gnome-themes-standard gtk2-engines-murrine
+# sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+# sudo apt-get update
+# sudo apt-get install -y arc-theme
+
 
 echo "--Finished, sourcing bashrc"
 source ~/.bashrc
